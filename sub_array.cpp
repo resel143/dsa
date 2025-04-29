e6b8cc84a71d8f7f1b7f8d54a13290b5b34c357b
unknown <reshul@braina.live>
2025-04-30 00:33:54 +0530

    Optimized maximum sum in Sub array of length K: static window

--- sub_array.cpp
+++ sub_array.cpp
@@ -29,6 +29,22 @@ int FindMaxSuminSubArrayofLengthK(int K, vector<int> inputArr){
 	return maxSum;
 }
 
+const OptimizedFindMaxSuminSubArrayofLengthK(int K, vector<int> inputArr){
+	int i=0, j=K-1, maxSum=0, sum=0;
+	for(int m=0;m<=j;m++){
+		sum+=inputArr[m];
+	}
+	maxSum = sum;
+	while(j<inputArr.size()){
+		i++;
+		j++;
+		sum -= inputArr[i-1];
+		sum+=inputArr[j];
+		maxSum = max(maxSum, sum);
+	}
+	
+	return maxSum;
+}
 
 int main(){
 	vector<int> inputArr;
@@ -42,7 +58,7 @@ int main(){
 	cout<<"Array you entered:"<<endl;
 	PrintArray(inputArr);
 	cout<<endl;
-	cout<<FindMaxSuminSubArrayofLengthK(4, inputArr);
+	cout<<OptimizedFindMaxSuminSubArrayofLengthK(4, inputArr);
 	return 0;
 	
 }
